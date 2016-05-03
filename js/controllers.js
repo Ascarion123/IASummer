c1edaadb955017d31694d8a4fffc57fee8fa6a55
Raymond Busby <raymondmbusby@gmail.com>
2016-05-02 17:29:48 -0700

    How long to eat summer?

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('contactInfoCtrl', function($scope) {
+
+})
+   
+.controller('scheduleCtrl', function($scope) {
+
+})
+    
---
+++ js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.contactInfo', {
+    url: '/contact',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/contactInfo.html',
+        controller: 'contactInfoCtrl'
+      }
+    }
+  })
+
+  .state('menu.schedule', {
+    url: '/schedule',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/schedule.html',
+        controller: 'scheduleCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/home')
+
+  
+
+});
